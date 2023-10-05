0c744a49af3b03e49b48c6eaeb48c930077e0b08
Nurdaulet <abdikaimovnurdaulet@gmail.com>
2023-10-05 15:36:59 +0600

    Start point, removed Storyboards, added necessery libraries (SnapKit, FireBase)

---
+++ KirisShygysApp/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  KirisShygysApp
+//
+//  Created by Нурдаулет on 05.10.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        window.windowScene = windowScene
+        window.rootViewController = UINavigationController(rootViewController: ViewController())
+        window.makeKeyAndVisible()
+        self.window = window
+
+    }
+}
+
---
+++ KirisShygysApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KirisShygysApp
+//
+//  Created by Нурдаулет on 05.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
