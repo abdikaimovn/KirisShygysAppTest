0f15a74ffd7a7b0b0c855fe176f77866481019af
Nurdaulet <abdikaimovnurdaulet@gmail.com>
2023-10-06 20:31:17 +0600

    Designed Onboarding page

---
+++ KirisShygysApp/Supporting/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  KirisShygysApp
+//
+//  Created by Нурдаулет on 05.10.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
