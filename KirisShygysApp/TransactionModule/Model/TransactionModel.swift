a0625b338b79cde7b55197d40a5aafa8cb072a99
Nurdaulet <abdikaimovnurdaulet@gmail.com>
2023-11-12 01:21:58 +0600

    Fixed bug with firebase * Now when the user adds new transaction the system will save it in relevant section (expense or income) then it also saves it in transaction section for further convinience to get receive data from Firebase * Changed TransactionModel, changes: Added two init functions. First to fill it. Second to receive data from firebase and convert it to the model

--- KirisShygysApp/TransactionModule/Model/TransactionModel.swift
+++ KirisShygysApp/TransactionModule/Model/TransactionModel.swift
@@ -13,10 +13,28 @@ struct TransactionModel {
     var transactionName: String
     var transactionDescription: String
     var transactionDate: String
+
+    // Инициализатор для преобразования данных из Firebase
+    init(data: [String: Any]) {
+        self.transactionAmount = data["amount"] as? Int ?? 0
+        self.transactionType = TransactionType(rawValue: data["type"] as? String ?? "") ?? .income
+        self.transactionName = data["name"] as? String ?? ""
+        self.transactionDescription = data["description"] as? String ?? ""
+        self.transactionDate = data["date"] as? String ?? ""
+    }
+
+    // Инициализатор для создания объекта из явных параметров
+    init(amount: Int, type: TransactionType, name: String, description: String, date: String) {
+        self.transactionAmount = amount
+        self.transactionType = type
+        self.transactionName = name
+        self.transactionDescription = description
+        self.transactionDate = date
+    }
 }
 
-enum TransactionType {
-    case income
-    case expense
+enum TransactionType: String {
+    case income = "income"
+    case expense = "expense"
 }
 
